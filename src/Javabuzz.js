2c3d81025b41ff6230a7c8cee7de4019ffbcd7e7
Jon Clarke <jonclarke1984@googlemail.com>
2022-04-11 12:05:22 +0100

    All done

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,13 +1,30 @@
 class Javabuzz {
   isDivisibleByThree(number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy(number, 3);
   }
 
   isDivisibleByFive(number) {
-    return (number % 5 === 0);
+    return this._isDivisibleBy(number, 5);
   }
 
   isDivisibleByFifteen(number) {
-    return (number % 15 === 0);
+    return this._isDivisibleBy(number, 15);
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return 'JavaBuzz';
+    }
+    if (this.isDivisibleByThree(number)) {
+      return 'Java';
+    }
+    if (this.isDivisibleByFive(number)) {
+      return 'Buzz';
+    }
+    return number;
   }
 }
